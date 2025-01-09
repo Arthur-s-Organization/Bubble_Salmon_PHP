86109cae81f6aa7f6480cff5b584f4541e8cf956
Nicolas <nicolas.descarp@outlook.fr>
2025-01-09 21:33:29 +0100

    feat : add UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace src\Controller;
+
+use src\Model\User;
+
+class UserController {
+    public function __construct()
+    {
+        header('Content-Type: application/json; charset=utf-8');
+    }
+
+    public function add() {
+        if ($_SERVER["REQUEST_METHOD"] != "POST") {
+            header("HTTP/1.1 405 Method Not Allowed");
+            return json_encode(["code" => 1, "Message" => "POST Attendu"]);
+        }
+
+        $data = file_get_contents("php://input");
+        $json = json_decode($data);
+
+
+        if (empty($json)) {
+            header("HTTP/1.1 400 Bad Request");
+            return json_encode(["code" => 1, "Message" => "Il faut des données"]);
+        }
+
+        if (!isset($json->firstname) || !isset($json->lastname)) {
+            header("HTTP/1.1 400 Bad Request");
+            return json_encode(["code" => 1, "Message" => "Il faut des données"]);
+        }
+
+        $user = new User();
+        $user->setFirstname($json->Name)
+            ->setLastname($json->Lastname)
+            ->setphone($json->Phone)
+            ->setBirthDate(new \DateTime($json->BirthDate))
+            ->setUsername($json->Username)
+            ->setPassword($json->Password)
+            ->setCreatedAt(new \DateTime($json->CreatedAt))
+            ->setUpdatedAt(new \DateTime($json->UpdatedAt));
+
+        $id = User::SqlAdd($user);
+        return json_encode(["code" => 0, "Message" => "StreetArt ajouté avec succès", "Id" => $id]);
+
+    }
+}
